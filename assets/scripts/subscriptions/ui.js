d07be51915eef7391e9280c615252d76deed73d7
Han Nguyen <hanvnguyen94@gmail.com>
2021-01-18 19:32:30 -0500

    Fixed bugs on ui - modified html and ui file - fixed display user name in nav bar - fixed bug on create success display accurate subscription

--- assets/scripts/subscriptions/ui.js
+++ assets/scripts/subscriptions/ui.js
@@ -2,13 +2,7 @@
 
 const store = require('./../store')
 
-// select DOM
-// const subInput = document.querySelector('.sub-input')
-// const subButton = document.querySelector('.sub-button')
 const subList = document.querySelector('.sub-list')
-
-// const editButton = document.getElementById('modal-editSub')
-// const deleteButton = document.getElementById('modal-deleteSub')
 
 const createSubSuccess = function (response) {
   $('.unauthenticated').hide()
@@ -17,43 +11,22 @@ const createSubSuccess = function (response) {
   $('.navbar').show()
   $('#message').text('New Subscription Created')
   $('.sub-list').show()
-  $('#sub-display').hide()
+  // $('#sub-display').hide()
   $('.sub-container').show()
   //  'store' the subscription
   let newSub = response.subscription
   store.subscription = newSub
-  // console.log('new sub data ', newSub)
-  // trying new thing to display
-  // const subsList = []
-  // subsList.push(newSub)
-  // subsList.forEach(singleSub => {
-  // create a whole div to display sub list
-  const subDiv = document.createElement('div')
-  subDiv.classList.add('sub')
-  // create list
-  newSub = document.createElement('li')
-  newSub.innerHTML = store.subscription.url
-  // save
-  newSub.classList.add('sub-item')
-  subDiv.appendChild(newSub)
-  // subDiv.classList.add('demo')
-  // create buttons
-  // const editButton = document.createElement('button')
-  // editButton.innerHTML = 'Edit'
-  //
-  // // adding class for styling
-  // editButton.classList.add('edit-btn')
-  // subDiv.appendChild(editButton)
-  // // Create delete button
-  // const deleteButton = document.createElement('button')
-  // deleteButton.innerHTML = 'Delete'
-  // deleteButton.classList.add('delete-btn')
-  // subDiv.appendChild(deleteButton)
-  // attach sub
-  subList.appendChild(subDiv)
-  // })
-  // $('.edit-btn').on('click', () => console.log('im here'))
-  // reset all forms
+
+  const subHTML = (`
+  <div class="demo">
+    <p>Website Address: ${newSub.url}</p>
+    <p>Start Date: ${newSub.start}</p>
+    <p>End Date: ${newSub.end}</p>
+    <p>ID: ${newSub._id}</p>
+  </div>`)
+
+  $('#sub-display').html(subHTML)
+
   $('form').trigger('reset')
 }
 
@@ -118,7 +91,6 @@ const getSubSuccess = function (response) {
   // console.log(response)
   const sub = response.subscription
 
-  // console.log('sub is ', sub)
   $('#message').text('Here is what we found')
   const subHTML = (`
     <div class="demo">
